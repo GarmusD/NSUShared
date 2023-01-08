0e4abc3512af8b839245fcc7280ff2ec35240b6d
GarmusD <garmusd@gmail.com>
2023-01-08 22:36:36 +0100

    Added property CommandId

--- DTO/NsuNet/NsuNetRequestBase.cs
+++ DTO/NsuNet/NsuNetRequestBase.cs
@@ -2,14 +2,18 @@
 
 namespace NSU.Shared.DTO.NsuNet
 {
+#nullable enable
     public class NsuNetRequestBase
     {
         [JsonProperty(JKeys.Generic.Target)]
         public string Target { get; set; } = string.Empty;
+
         [JsonProperty(JKeys.Generic.Action)]
         public string Action { get; set; } = string.Empty;
+
         [JsonProperty(JKeys.Generic.CommandID)]
-        public string CommandId { get; set; }
+        public string? CommandId { get; set; }
+
         [JsonProperty(JKeys.Generic.ResponseRequired)]
         public bool ResponseRequired { get; set; }
     }
