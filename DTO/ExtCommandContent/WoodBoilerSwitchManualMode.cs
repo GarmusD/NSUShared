2c432ade2ffe5a9488f688ab3f8977199fd35e72
GarmusD <garmusd@gmail.com>
2023-01-08 22:35:00 +0100

    Merged Ladomat and ExhaustFan

---
+++ DTO/ExtCommandContent/WoodBoilerSwitchManualMode.cs
@@ -0,0 +1,14 @@
+﻿namespace NSU.Shared.DTO.ExtCommandContent
+{
+    public struct WoodBoilerSwitchManualMode
+    {
+        public string WoodBoilerName { get; }
+        public string SwitchTarget { get; }
+
+        public WoodBoilerSwitchManualMode(string wbName, string switchTarget)
+        {
+            WoodBoilerName = wbName;
+            SwitchTarget = switchTarget;
+        }
+    }
+}
